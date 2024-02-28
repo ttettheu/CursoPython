6349215cff58f880c69265b7f93031d5b352fdac
matheus <matheusalcantarapvh@gmail.com>
2024-02-28 17:42:42 -0300

    Aula 28

---
+++ loop_while.py
@@ -0,0 +1,47 @@
+"""
+Loop while
+
+Forma geral
+
+while expressão_booleana:
+    //execução do loop
+
+O bloco do while será repetido enquanto a expressão booleana for verdadeira.
+
+Expressão Booleana é toda expressão onde o resultado é verdadeiro ou falso.
+
+Exemplo:
+
+num = 5
+
+num < 5
+
+# Exemplo 1
+
+numero = 1
+
+while numero < 10:
+    print(numero)
+    numero += 1
+
+# OBS: Em um loop while é importante que cuidemos do critério de parada para não causar um loop infinito.
+
+# C ou Java
+
+while(expressão){
+    //execução
+}
+
+# do while (C ou Java)
+
+do {
+    //expressão
+}while(expressão);
+"""
+
+# Exemplo 2
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Já achou Jéssica? ')
