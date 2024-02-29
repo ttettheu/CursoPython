9202ae5576f3dc26faa584c953ead7781e3d7026
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 11:05:17 -0300

    Exercício 028

--- exercicios/secao_4/ex_028.py
+++ exercicios/secao_4/ex_028.py
@@ -2,3 +2,10 @@
 Faça a leitura de três valores e apresente como resultado a soma dos quadrados dos
 três valores lidos.
 """
+
+valor1 = float(input('Digite um valor: '))
+valor2 = float(input('Digite um valor: '))
+valor3 = float(input('Digite um valor: '))
+soma = valor1 + valor2 + valor3
+
+print(f'A soma dos valores é {soma}.')
