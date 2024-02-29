75192a1d11e69537bbe9ad9d85f4bd7d07a2a79f
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 11:01:29 -0300

    Exercício 027

--- exercicios/secao_4/ex_027.py
+++ exercicios/secao_4/ex_027.py
@@ -3,3 +3,7 @@ Leia um valor de área em hectares e apresente-o convertido em metros quadrados 
 A fórmula de conversão é: M = H * 10000, sendo M a área em metros quadrados e H a
 área em hectares
 """
+
+H = float(input('Digite o valor da área em hectares: '))
+M = H * 10000
+print(f'{H}ha equivalem a {M:.0f}m².')
