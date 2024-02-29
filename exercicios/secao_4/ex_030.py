524468ac764848e28cf70059c5cf76c7e61164bc
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 11:17:21 -0300

    Exercício 030

--- exercicios/secao_4/ex_030.py
+++ exercicios/secao_4/ex_030.py
@@ -2,3 +2,8 @@
 Leia um valor em real e a cotação do dólar. Em seguida, imrpima o valor correspondente
 em dólares.
 """
+
+real = float(input('Digite o valor em R$ (apenas números): '))
+dolar = real / 4.98
+
+print(f'R$ {real} equivalem a U$ {dolar:.2f}')
