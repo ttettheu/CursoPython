f37cc6479c42571eae5a12e3a05918bcb21b7080
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 09:17:54 -0300

    Exercício 013

--- exercicios/secao_4/ex_013.py
+++ exercicios/secao_4/ex_013.py
@@ -2,3 +2,8 @@
 Leia uma distância em quilômetros e apresente-a convertida em milhas.
 A fórmula de conversão é: M = K / 1,61, sendo K a distância em quilômetros e M em milhas.
 """
+
+K = float(input('Digite a distância em quilômetros: '))
+M = K / 1.61
+
+print(f'{K}km são {M}milhas')
