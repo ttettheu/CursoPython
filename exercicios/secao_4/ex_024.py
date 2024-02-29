362de3ae912ea5e44d3c317637e2ad0164ca1e04
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 10:54:36 -0300

    Exercício 024

--- exercicios/secao_4/ex_024.py
+++ exercicios/secao_4/ex_024.py
@@ -3,3 +3,6 @@ Leia um valor de área em metros quadrados m² e apresente-o convertido em acres
 fórmula de conversão é: A = M * 0,000247, sendo M a área em metros quadrados e A
 a área em acres.
 """
+
+M = float(input('Digite o valor em metros quadrados m²: '))
+print(f'{M} m² equivalem a {M * 0.000247} acres')
