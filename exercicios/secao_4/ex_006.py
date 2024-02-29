b196f68f50dfd74f333f3783b0b409b95edbd35e
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 08:42:42 -0300

    Exercício 006

--- exercicios/secao_4/ex_006.py
+++ exercicios/secao_4/ex_006.py
@@ -1,5 +1,10 @@
 """
 Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit.
-A fórmula de conversão é: F = C*(9.0/5.0)+32,0, sendo F a temperatura em Fahrenheit
+A fórmula de conversão é: F = C*(9.0/5.0)+32.0, sendo F a temperatura em Fahrenheit
 e C a temperatura em Celsius.
 """
+
+C = float(input('Digite a temperatura em ºC: '))
+F = (C * (9.0 / 5.0) + 32.0)
+
+print(f'{C} ºC são {F} ºF')
