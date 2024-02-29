ef4dcf666a0932f74ddf46ba20f96481db47db54
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 08:29:16 -0300

    Exercício 004

--- exercicios/secao_4/ex_004.py
+++ exercicios/secao_4/ex_004.py
@@ -1,3 +1,7 @@
 """
 Leia um número real e imprima o resultado do quadrado desse número.
 """
+
+numeroReal = float(input('Digite um númeor real: '))
+
+print(f'O quadrado para o número {numeroReal} é {numeroReal**2}')
