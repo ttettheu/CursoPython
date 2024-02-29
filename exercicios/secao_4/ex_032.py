e3d6fd7215d7fd74f578d8cbf3b96e7c63ddeceb
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 15:36:21 -0300

    Exercício 032

--- exercicios/secao_4/ex_032.py
+++ exercicios/secao_4/ex_032.py
@@ -2,3 +2,10 @@
 Leia um número inteiro e imprima a soma do sucessor de seu triplo com o antecessor de
 seu dobro.
 """
+
+numero = int(input('Digite um número: '))
+sucessor_triplo = ((numero*3)+1)
+antecessor_dobro = ((numero*2)-1)
+soma = sucessor_triplo + antecessor_dobro
+
+print(f'Sucessor do seu triplo: {sucessor_triplo}\nAntecessor do seu dobro: {antecessor_dobro}\nSoma: {soma}')
