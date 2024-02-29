0c5535c639f4a891b304c152d0e6d1e6898b20c0
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 20:00:09 -0300

    Exercício 041

--- exercicios/secao_4/ex_041.py
+++ exercicios/secao_4/ex_041.py
@@ -3,3 +3,16 @@ Faça um programa que leia o valor da hora de trabalho (em reais) e número de h
 trabalhadas no mês. Imprima o valor a ser pago ao funcionário, adicionando 10% sobre
 o valor calculado.
 """
+
+valor_hora = float(input('Qual o valor da hora de trabalho (R$)? '))
+horas_trabalhadas = float(input('Quantas horas o funcionário trabalho no mês? '))
+total_bruto = valor_hora * horas_trabalhadas
+acrescimo = total_bruto * 10 / 100
+
+print(f'''
+Valor da Hora: R$ {valor_hora:.2f}
+Horas Trabalhadas: R$ {horas_trabalhadas:.2f}
+Total Bruto: R$ {total_bruto:.2f}
+Acrescimo (10%): R$ {acrescimo:.2f}
+Valor Líquido: R$ {total_bruto + acrescimo:.2f}
+''')
