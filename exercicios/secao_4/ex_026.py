abbfb2c914135044125a0659549af3ef2b14b080
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 10:59:22 -0300

    Exercício 026

--- exercicios/secao_4/ex_026.py
+++ exercicios/secao_4/ex_026.py
@@ -3,3 +3,7 @@ Leia um valor de área em metros quadrados m² e apresente-o convertido em hecta
 A fórmula de conversão é: H = M * 0,0001, sendo M a área em metros quadrados e H a
 área em hectares
 """
+
+M = float(input('Digite o valor da área em metros quadrados m²: '))
+H = M * 0.0001
+print(f'{M}m² equeivalem a {H}ha.')
