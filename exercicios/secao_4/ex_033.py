fdaad63960cab33756c31b53126589d94994b7e8
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 15:39:42 -0300

    Exercício 033

--- exercicios/secao_4/ex_033.py
+++ exercicios/secao_4/ex_033.py
@@ -1,3 +1,7 @@
 """
 Leia o tamanho do lado de um quadrado e imprima como resultado a sua área.
 """
+
+lado = float(input('Digite o valor do lado: '))
+
+print(f'O quadrado tem uma área de {lado*2}')
