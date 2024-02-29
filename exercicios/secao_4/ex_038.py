3c6fda52e69666ac8657fb6ecc75a9616768f738
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 16:27:08 -0300

    Exercício 038

--- exercicios/secao_4/ex_038.py
+++ exercicios/secao_4/ex_038.py
@@ -2,3 +2,8 @@
 Leia o salário de um funcionário. Calcule e imprima o valor do novo salário, onde que
 ele recebeu um aumento de 25%
 """
+
+salario = float(input('Digite o valor do salário: '))
+aumento = (salario * 25) / 100
+
+print(f'O salário com aumento de 25% é R${salario + aumento:.2f}')
