58593d7cf94679c3a410ffbdc7e22398d2caf713
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 08:54:09 -0300

    Exercício 008

--- exercicios/secao_4/ex_008.py
+++ exercicios/secao_4/ex_008.py
@@ -3,3 +3,8 @@ Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius.
 A fórmula de conversão é: C = K - 273,15, sendo C a temperatura em Celsius
 e K a temperatura em Kelvin.
 """
+
+K = float(input('Digite a temperatura em ºK: '))
+C = K - 273.15
+
+print(f'{K}ºF são {C:.2f}ºC')
