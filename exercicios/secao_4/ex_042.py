6649aff8887163362878d14e7d3679d4eb95368a
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 20:05:34 -0300

    Exercício 042

--- exercicios/secao_4/ex_042.py
+++ exercicios/secao_4/ex_042.py
@@ -3,3 +3,15 @@ Recebe o salário-base de um funcionário. Calcule e imprima o salário a recebe
 se que esse funcionário tem uma gratificação de 5% sobre o salário-base. Além disso,
 ele paga 7% de imposto sobre o salário-base.
 """
+
+salario_base = float(input('Digite o Salário-Base do funcionário: '))
+gratificacao = (salario_base * 5) / 100
+imposto = (salario_base * 7) / 100
+salario_liquido = salario_base + gratificacao - imposto
+
+print(f'''
+Salário-Base: R$ {salario_base:.2f}
+Gratificação: + R$ {gratificacao:.2f}
+Imposto: - R$ {imposto:.2f}
+Salário Líquido: R$ {salario_liquido:.2f}
+''')
