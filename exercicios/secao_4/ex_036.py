a8c403cd7ed42dc360fdec8e5dca6ce8d884b24d
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 16:20:51 -0300

    Exercício 036

--- exercicios/secao_4/ex_036.py
+++ exercicios/secao_4/ex_036.py
@@ -2,3 +2,9 @@
 Leia a altura e o raio de um cilindro circular e imprima o volume do cilindro. O volume
 de um cilindo circular é calculado por meio da seguinte fórmula: V = π * raio² * altura, onde π = 3.141592.
 """
+
+altura = float(input('Digite a altura: '))
+raio = float(input('Digite o raio: '))
+volume = 3.141592 * raio ** 2 * altura
+
+print(f'O volume do cilindro é {volume}')
--- exercicios/secao_4/ex_045.py
+++ exercicios/secao_4/ex_045.py
@@ -2,3 +2,5 @@
 Faça um programa para converter uma letra maiúscula em letra minúscula. Use a tabela
 ASCII para resolver o problema.
 """
+
+
