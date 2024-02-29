9ab96db152cd12a3b6988cb9698fff06225be93b
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 11:10:05 -0300

    Exercício 029

--- exercicios/secao_4/ex_029.py
+++ exercicios/secao_4/ex_029.py
@@ -1,3 +1,11 @@
 """
 Leia quatro notas, calcule a média aritmética e imprima o resultado.
 """
+
+nota1 = float(input('Nota 1: '))
+nota2 = float(input('Nota 2: '))
+nota3 = float(input('Nota 3: '))
+nota4 = float(input('Nota 4: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f'Sua média é {media}.')
