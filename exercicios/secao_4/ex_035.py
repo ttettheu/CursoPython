5392aa2f0418a4cae91fc44c26b2929dc6f66705
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 16:15:58 -0300

    Exercício 035

--- exercicios/secao_4/ex_035.py
+++ exercicios/secao_4/ex_035.py
@@ -1,5 +1,14 @@
 """
 Sejam a e b os catetos de um triângulo, onde a hipotenusa é obtida pela equação:
-hipotenusa = √a² + b². Faça um programa que receba os valores de a e b e calcule
+hipotenusa = √ a² + b². Faça um programa que receba os valores de a e b e calcule
 o valor da hipotenusa através da equação. Imprima o resultado dessa operação.
 """
+
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+hipotenusa = (a ** 2 + b ** 2) ** (1 / 2)
+
+print(f'''O valor da hipotenusa para
+a = {a}
+b = {b}
+hipotenusa é {hipotenusa}''')
