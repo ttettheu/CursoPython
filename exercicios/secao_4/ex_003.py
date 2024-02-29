addafeff728c6165a5f94068a9df64009df2b34f
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 08:23:56 -0300

    Exercício 003

--- exercicios/secao_4/ex_003.py
+++ exercicios/secao_4/ex_003.py
@@ -1,3 +1,10 @@
 """
 Peça ao usuário para digitar três valores inteiros e imprima a soma deles.
 """
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+valor3 = int(input('Digite o terceiro valor: '))
+soma = valor1 + valor2 + valor3
+
+print(f'A soma dos valores digitados é {soma}')
