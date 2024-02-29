ff7f162a5b8df9b6f45984ae029542d78efa943f
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 10:51:54 -0300

    Exercício 022

--- exercicios/secao_4/ex_022.py
+++ exercicios/secao_4/ex_022.py
@@ -2,3 +2,6 @@
 Leia um valor de comprimento em jardas e apresente-o convertido em metros. A fórmula
 de conversão é: M = 0,91 * J, sendo J o comprimento em jardas e M o comprimento em metros.
 """
+
+J = float(input('Digite o valor do comprimento em jardas: '))
+print(f'{J}yd equivalem a {J * 0.91}m')
