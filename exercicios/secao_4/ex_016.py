6f52b0e4a69515e9601983498ebca8b4e68a54c2
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 10:27:15 -0300

    Exercício 016

--- exercicios/secao_4/ex_016.py
+++ exercicios/secao_4/ex_016.py
@@ -3,3 +3,8 @@ Leia um valor de comprimento em polegadas e apresente-o convertido em centímetr
 A fórmula de conversão é: C = P * 2.54, sendo C o comprimento em centímetros e P o
 comprimento em polegadas.
 """
+
+P = float(input('Digite o valor de comprimento em polegadas: '))
+C = P * 2.54
+
+print(f'{P} pol equivalem a {C} cm.')
