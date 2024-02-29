143a455ad0897c4a95eccf012d693b14c2475686
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 15:43:04 -0300

    Exercício 034

--- exercicios/secao_4/ex_034.py
+++ exercicios/secao_4/ex_034.py
@@ -2,3 +2,8 @@
 Leia o valor do raio de um círculo e calcule e imprima a área do círculo correspondente.
 A área do círculo é π * raio², considere π = 3.141592.
 """
+
+raio = float(input('Digite o valor do raio: '))
+area_circulo = 3.141592 * raio ** 2
+
+print(f'O círculo tem uma área de {area_circulo}')
