c57e2cda6271351b296d5292af3cafc3c0ea4bf1
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 09:07:04 -0300

    Exercício 011

--- exercicios/secao_4/ex_011.py
+++ exercicios/secao_4/ex_011.py
@@ -2,3 +2,8 @@
 Leia uma velocidade em m/s (metros por segundo) e apresente-a convertida em km/h (quilômetros por hora).
 A fórmula de conversão é: K = M * 3,6, sendo K a velocidade em km/h e M em m/s.
 """
+
+M = float(input('Digite a velocidade em m/s: '))
+K: float = M * 3.6
+
+print(f'{M} em m/s são {K}')
