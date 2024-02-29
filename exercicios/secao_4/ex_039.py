8cabd38572599acaeab083f06c1dba7589c46d89
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 16:37:37 -0300

    Exercício 039

--- exercicios/secao_4/ex_039.py
+++ exercicios/secao_4/ex_039.py
@@ -6,3 +6,15 @@ Sendo que da quantia total:
     - O terceiro receberá o restante:
 Calcule e imprima a quantia ganhada por cada um dos ganhadores.
 """
+
+premio = 780_000
+primeiro = (premio * 46) / 100
+segundo = (premio * 32) / 100
+terceiro = premio * (100 - 46 - 32) / 100
+
+print(f'''
+Um prêmio de R$ 780.000,00 dividido para três ganhadores é:
+Primeiro (46%): R$ {primeiro:.2f}
+Segundo (32%): R$ {segundo:.2f}
+Terceiro ({100 - 46 - 32}%): R$ {terceiro:.2f} 
+''')
