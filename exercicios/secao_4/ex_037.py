2f28fae713285352c79c8bd783cf901c085b621e
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 16:24:21 -0300

    Exercício 037

--- exercicios/secao_4/ex_037.py
+++ exercicios/secao_4/ex_037.py
@@ -2,3 +2,8 @@
 Faça um programa que leia o valor de um produto e imprima o valor com desconto, tendo
 em vista que o desconto foi de 12%
 """
+
+produto = float(input('Digite o valor do produto: '))
+desconto = (produto * 12) / 100
+
+print(f'Valor do produto com desconto de 12% é {produto - desconto}')
