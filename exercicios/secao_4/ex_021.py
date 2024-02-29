cd6d7d8e0425b41e84d812402a2f76205add4cc4
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 10:42:34 -0300

    Exercício 021

--- exercicios/secao_4/ex_021.py
+++ exercicios/secao_4/ex_021.py
@@ -2,3 +2,7 @@
 Leia um valor de massa em libras e apresente-o convertido em quilograms. A fórmula
 de conversão é: K = L * 0,45, sendo K a massa em quilogramas e L a massa em libras.
 """
+
+L = float(input('Digite o valor da massa em libras: '))
+
+print(f'{L}lb equivalem a {L * 0.45}')
