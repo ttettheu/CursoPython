5a499649118d4fb7873a59d4a30bb2f884f3f7c2
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 09:20:50 -0300

    Exercício 014

--- exercicios/secao_4/ex_014.py
+++ exercicios/secao_4/ex_014.py
@@ -2,3 +2,8 @@
 Leia um ângulo em graus e apresente-o convertido em radianos.
 A fórmula de conversão é: R = G * π/180, sendo G o ângulo em graus e R em radianos e π = 3.14.
 """
+
+G = float(input('Digite o ângulo em graus: '))
+R = G * 3.14 / 180
+
+print(f'{G}º são {R} radianos')
