8044c2f3bb2aaeabd8358d0a7bb1e2a0dce0467d
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 16:32:23 -0300

    Exercício 049

--- exercicios/secao_4/ex_049.py
+++ exercicios/secao_4/ex_049.py
@@ -3,3 +3,16 @@ Faça um programa que leia o horário (hora, minuto e segundo) de início e a du
 segundos, de uma experiência biológica. O programa deve resultar com o novo horário
 (hora, minuto e segundo) do término da mesma.
 """
+
+print('Digite o horário de início da experiência abaixo:')
+
+hora = int(input('Hora: '))
+minuto = int(input('Minuto: '))
+segundo = int(input('Segundo: '))
+duracao = int(input('Duração da experiência (segundos): '))
+
+segundoFinal = (segundo + duracao) % 60
+minutoFinal = (minuto + (segundo + duracao) // 60) % 60
+horaFinal = (hora + (minuto + (segundo + duracao) // 60) // 60) % 24
+
+print(f'Fim do evento: {horaFinal}h {minutoFinal}min e {segundoFinal} segundos')
