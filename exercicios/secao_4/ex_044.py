0e5d01098c9a9a1d0e290b784d31d7ec3f3c8651
matheus <matheusalcantarapvh@gmail.com>
2024-02-29 21:05:31 -0300

    Exercício 044

--- exercicios/secao_4/ex_044.py
+++ exercicios/secao_4/ex_044.py
@@ -3,3 +3,8 @@ Receba a altura do degrau de uma escada e a altura que o usuário deseja alcanç
 subindo a escada. Calcule e mostre quantos degraus o usuário deverá subir para atingir
 seu objetivo.
 """
+
+altura_degrau = float(input('Digite a altura do degrau (cm): '))
+altura_desejada = float(input('Digite a altura que deseja alcançar (cm): '))
+
+print(f'Você precisará subir {altura_desejada / altura_degrau:.0f} degraus para alcançar a altura de {altura_desejada:.0f}')
