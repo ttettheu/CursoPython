7a44e436343d7947355161d31a37a32edfadb4f0
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 16:05:30 -0300

    Exercício 047

--- exercicios/secao_4/ex_047.py
+++ exercicios/secao_4/ex_047.py
@@ -1,3 +1,16 @@
 """
 Leia um número inteiro de 4 dígitos (de 1000 a 9999) e imprima 1 dígito por linha.
 """
+
+numero = int(input('Digite um número (de 1000 a 9999): '))
+list = list(str(numero))
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+
+"""
+Não gostei desse código, imagina fazer para vários números. Espero que tenha uma solução melhor,
+verei nas próximas aulas.
+"""
