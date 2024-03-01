42baf14abf1378e60f9b26f10ab84dbef8ae1708
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 17:46:37 -0300

    Exercício 052

--- exercicios/secao_4/ex_052.py
+++ exercicios/secao_4/ex_052.py
@@ -4,3 +4,25 @@ porcionalmente ao valor que cada deu para a realização da aposta. Faça um pro
 que leia quanto cada apostador investiu, o valor do prêmio, e imprima quanto cada um
 ganharia do prêmio com base no valor investido.
 """
+
+print('Digite os valores que cada um apostou:')
+heitor = float(input('Heitor: '))
+uriel = float(input('Uriel: '))
+matheus = float(input('Matheus: '))
+aposta = (heitor + uriel + matheus)
+print(f'Total apostado: {aposta}')
+
+print('\nDigite o valor do Prêmio:')
+premio = float(input('Prêmio: '))
+
+heitorGanha = (((heitor * aposta) / 100) * premio) / 100
+urielGanha = (((uriel * aposta) / 100) * premio) / 100
+matheusGanha = (((matheus * aposta) / 100) * premio) / 100
+
+print(f'''
+Valores proporcionais a aposta de cada ganhador:
+
+Heitor ganhou R$ {heitorGanha:.2f}
+Uriel ganhou R$ {urielGanha:.2f}
+Matheus ganhou R$ {matheusGanha:.2f} 
+''')
