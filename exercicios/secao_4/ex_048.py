608ffcaa31c949964e134c12283abc130445ab9a
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 16:13:45 -0300

    Exercício 048

--- exercicios/secao_4/ex_048.py
+++ exercicios/secao_4/ex_048.py
@@ -1,3 +1,11 @@
 """
 Leia um valor inteiro em segundos, e imprima-o em horas, minutos e segundos.
 """
+
+numero = int(input('Digite um valor em segundos: '))
+
+print(f'''
+Horas: {numero / 3600:.2f}
+Minutos: {numero / 60:.2f}
+Segundos: {numero}
+''')
