7d1dba49240d7e384ac83fefed4d568722c54492
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 16:56:37 -0300

    Exercício 051

--- exercicios/secao_4/ex_051.py
+++ exercicios/secao_4/ex_051.py
@@ -2,3 +2,9 @@
 Escreva um programa que leia as coordenadas x e y de pontos no R² e calcule sua
 distância de origem (0,0).
 """
+
+x = float(input('Digite o valor de x: '))
+y = float(input('Digite o valor de y: '))
+resultado = ((x ** 2) + (y ** 2) ** (1 / 2))
+
+print(f'A distância de origem é: {resultado}.')
