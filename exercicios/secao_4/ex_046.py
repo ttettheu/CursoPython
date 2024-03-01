0e5719f5fc5a09a65d38953437dee90ac896aaae
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 14:41:00 -0300

    Exercício 046

--- exercicios/secao_4/ex_046.py
+++ exercicios/secao_4/ex_046.py
@@ -5,3 +5,16 @@ Gere outro número formado pelos dígitos invertidos do número lido. Exemplo:
 NúmeroLido = 123
 NúmeroGerado = 321
 """
+
+numeroLido: int = input('Digite um número (de 100 a 999): ')
+numeroGerado: str = numeroLido[:3]
+
+print(f'''
+Número Lido = {numeroLido}
+Número Gerado = {numeroGerado[::-1]}
+''')
+
+"""
+Queria muito ter colocado uma limitação no input, pelas aulas não encontrei essa opção e,
+nem sei se é possível. Mas por enquanto é isso.
+"""
