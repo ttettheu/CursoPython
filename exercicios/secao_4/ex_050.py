a4f46ff8df3b3e115c2832d5442fd26fa7bba524
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 16:37:50 -0300

    Exercício 050

--- exercicios/secao_4/ex_050.py
+++ exercicios/secao_4/ex_050.py
@@ -2,3 +2,8 @@
 Implemente um programa que calcule o ano de nascimento de uma pessoa a partir de
 sua idade e do ano atual.
 """
+
+idade = int(input('Digite a sua idade: '))
+anoAtual = int(input('Digite o ano atual: '))
+
+print(f'Você nasceu no ano {anoAtual - idade}.')
