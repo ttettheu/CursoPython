ee3658349a35fb0c554d574bf53faa1d4186944e
matheus <matheusalcantarapvh@gmail.com>
2024-03-01 21:09:38 -0300

    Exercício 053

--- exercicios/secao_4/ex_053.py
+++ exercicios/secao_4/ex_053.py
@@ -1,5 +1,12 @@
 """
-Faça um programa para ler as dimensões de um terreno (comprimento c e largulal),
+Faça um programa para ler as dimensões de um terreno (comprimento c e largura l),
 bem como o preço do metro de tela p. Imprima o custo para cercar este mesmo terreno
 com tela.
 """
+
+c = float(input('Digite o comprimento: '))
+l = float(input('Digite a largura: '))
+p = float(input('Digite o valor da tela: '))
+custo = (c * 2) + (l * 2) * p
+
+print(f'O custo para um perímetro de {c * 2 + l * 2}m é R$ {custo}.')
