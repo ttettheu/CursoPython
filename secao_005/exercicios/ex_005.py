ce8ce1993f512e90a876b01cdb9b3c5b2753bb8c
matheus <matheusalcantarapvh@gmail.com>
2024-03-03 19:21:23 -0300

    Exercício 005

--- secao_005/exercicios/ex_005.py
+++ secao_005/exercicios/ex_005.py
@@ -2,3 +2,10 @@
 Faça um programa que receba um número inteiro e verifique se este número é par ou
 ímpar.
 """
+
+numero = int(input('Digite um número inteiro: '))
+
+if (numero % 2) == 0:
+    print(f'O número {numero} é par.')
+else:
+    print(f'O número {numero} é impar.')
