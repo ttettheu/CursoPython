ee429fa268cf964c500b8af50e4b68abee58bedb
matheus <matheusalcantarapvh@gmail.com>
2024-03-03 17:28:19 -0300

    Exercício 003

--- secao_005/exercicios/ex_003.py
+++ secao_005/exercicios/ex_003.py
@@ -2,3 +2,10 @@
 Leia um número real. Se o número for positivo imprima a raiz quadrada. Do contrário,
 imprima o número ao quadrado.
 """
+
+numero = float(input('Digite um número real: '))
+
+if numero > 0:
+    print(f'A raiz quadrade de {numero} é {numero ** (1/2)}')
+else:
+    print(f'O número {numero} ao quadrado é {numero ** 2}')
