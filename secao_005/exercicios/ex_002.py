ccfa3502dcfb119fb4af03569c4878c22d87f06f
matheus <matheusalcantarapvh@gmail.com>
2024-03-03 17:22:50 -0300

    Exercício 002

--- secao_005/exercicios/ex_002.py
+++ secao_005/exercicios/ex_002.py
@@ -3,3 +3,10 @@ Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a
 quadrada do número. Se o número for negativo, mostre uma mensagem dizendo que o
 número é inválido.
 """
+
+numero = float(input('Digite um número: '))
+
+if numero > 0:
+    print(f'{numero} é positivo e sua raiz quadrada é {numero ** (1/2)}')
+else:
+    print(f'{numero} é inválido.')
