34aded12dbf515aea76efbe1435c0cfc18c36656
matheus <matheusalcantarapvh@gmail.com>
2024-03-03 19:34:50 -0300

    Exercício 007

--- secao_005/exercicios/ex_007.py
+++ secao_005/exercicios/ex_007.py
@@ -2,3 +2,13 @@
 Faça um programa que receba dois números e mostre o maior. Se por acaso, os dois
 números forem iguais, imprima a mensagem 'Números iguais'.
 """
+
+numero1 = float(input('Digite um número: '))
+numero2 = float(input('Digite outro número: '))
+
+if numero1 > numero2:
+    print(f'O maior é o número {numero1}')
+if numero1 == numero2:
+    print(f'Números iguais')
+else:
+    print(f'O maior é o número {numero2}')
