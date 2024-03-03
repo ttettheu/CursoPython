c2f5ea1b1d815d7249122562f163abd3f2763006
matheus <matheusalcantarapvh@gmail.com>
2024-03-03 17:12:40 -0300

    Exercício 001

--- secao_005/exercicios/ex_001.py
+++ secao_005/exercicios/ex_001.py
@@ -1,3 +1,10 @@
 """
 Faça um programa que receba dois número e mostre qual deles é o maior.
 """
+numero1 = float(input('Digite um número: '))
+numero2 = float(input('Digite outro número: '))
+
+if numero1 > numero2:
+    print(f'O maior é {numero1}')
+else:
+    print(f'O maior é {numero2}')
