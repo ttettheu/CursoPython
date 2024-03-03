a7208c5da1aa21f071a089846219924ef2adea01
matheus <matheusalcantarapvh@gmail.com>
2024-03-03 20:19:30 -0300

    Exercício 008

--- secao_005/exercicios/ex_008.py
+++ secao_005/exercicios/ex_008.py
@@ -1,6 +1,15 @@
 """
 Faça um programa que leia 2 notas de um aluno, verifique se as notas são válidas e
 exiba na tela a média destas notas. Uma nota válida dever ser, obrigatoriamente, um
-valor entre 0.0 e 10.0, onde caso a noota não possua um valor válido, este fato deve ser
+valor entre 0,0 e 10,0, onde caso a nota não possua um valor válido, este fato deve ser
 informado ao usuário e o programa termina.
 """
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+if nota1 <= 0 or nota1 >= 10 and nota2 <= 0 or nota2 >= 10:
+    print(f'Nota não válida, digite as notas entre 0 e 10,00')
+else:
+    media = (nota1 + nota2) / 2
+    print(f'Sua média é {media}')
