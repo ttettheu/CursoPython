e36213ff470a51a3f18e0f2948d4557e7556c846
matheus <matheusalcantarapvh@gmail.com>
2024-03-03 19:04:33 -0300

    Exercício 004

--- secao_005/exercicios/ex_004.py
+++ secao_005/exercicios/ex_004.py
@@ -3,3 +3,8 @@ Faça um programa que leia um número e, caso ele seja positivo, calcule e mostr
     - O número digitado ao quadrado
     - A raiz quadrada do número digitado
 """
+
+numero = float(input('Digite um número: '))
+
+if numero > 0:
+    print(f'{numero} ao quadrado é igual a {numero ** 2} e a sua raiz é {numero ** (1/2)}')
