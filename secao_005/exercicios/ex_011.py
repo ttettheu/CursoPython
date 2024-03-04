c0a83e1aea043409687aec008cc952b9a8db996a
matheus <matheusalcantarapvh@gmail.com>
2024-03-04 09:15:23 -0300

    Exercício 011

--- secao_005/exercicios/ex_011.py
+++ secao_005/exercicios/ex_011.py
@@ -4,3 +4,17 @@ soma de todos os seus algarismos. Por exemplo, ao número 251 corresponderá o v
 8 (2 + 5 + 1). Se o número lido não for maior do que zero, o programa terminará com a
 mensagem 'Número inválido'.
 """
+
+numero = int(input('Digite um número: ')) # Entrada de um número Inteiro
+strNumero = str(numero) # Cast de interiro para String
+soma = 0
+
+for i in strNumero:
+    soma += int(i)
+print(soma)
+
+"""
+Cara, o que eu fiz aqui, acho que foi uma gambiarra kkk, mas deu certo!
+Talvez tenha algo mais fácil, onde não precise fazer o cast de interio
+para string mas, se tem ainda não aprendi.
+"""
