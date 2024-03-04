2b0eb67a46a2b41635be5dc0d41d18ccef48d2f6
matheus <matheusalcantarapvh@gmail.com>
2024-03-04 09:56:55 -0300

    Exercício 013

--- secao_005/exercicios/ex_013.py
+++ secao_005/exercicios/ex_013.py
@@ -4,3 +4,13 @@ a segunda prova têm peso 1 e a terceira tem peso 2. Ao final, mostrar a média 
 e indicar se o aluno foi aprovado ou reprovado. A nota para aprovação deve ser igual ou
 superior a 60 pontos.
 """
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+media = ((nota1 * 1) + (nota2 * 1) + (nota3 * 2)) / 4
+
+if media >= 60:
+    print(f'{media} - Aprovado')
+else:
+    print(f'{media} - Reprovado')
