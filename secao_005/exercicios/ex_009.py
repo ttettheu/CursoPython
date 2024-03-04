5d84961ce87ccb4895a9146408607338ccb24f1d
matheus <matheusalcantarapvh@gmail.com>
2024-03-04 00:24:51 -0300

    Exercício 009

--- secao_005/exercicios/ex_009.py
+++ secao_005/exercicios/ex_009.py
@@ -3,3 +3,12 @@ Leia o salário de um trabalhador e o valor da prestação de um empréstimo. Se
 prestação for maior que 20% do salário imprima: 'Empréstimo não concedido', caso
 contrário imprima: 'Emprestimo concedido.'.
 """
+
+salario = float(input('Digite o salário: '))
+prestacao = float(input('Digite o valor da prestação: '))
+porcentagem = (salario * 20) / 100
+
+if prestacao > porcentagem:
+    print('Esmprestimo não concedido.')
+else:
+    print('Emprestimo concedido.')
