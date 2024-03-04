fd198a26666c1ac66405a1c814e15c05d74042e2
matheus <matheusalcantarapvh@gmail.com>
2024-03-04 00:41:05 -0300

    Exercício 010

--- secao_005/exercicios/ex_010.py
+++ secao_005/exercicios/ex_010.py
@@ -1,6 +1,16 @@
 """
-Faça um program que reeba a altura e o sexo de uma pessoa e calcule e mostre seu
+Faça um program que receba a altura e o sexo de uma pessoa e calcule e mostre seu
 peso ideal, utilizando as seguintes fórmulas (onde h corresponde à altura):
     - Homenes: (72.7 * h) - 58
     - Mulheres: (62.1 * h) - 44.7
 """
+
+altura = float(input('Digite a altura: '))
+sexo = input('Digite seu sexo: ')
+
+if sexo.lower() == 'homem':
+    pesoIdeal = (72.7 * altura) - 58
+    print(f'Seu peso ideal é {pesoIdeal}')
+elif sexo.lower() == 'mulher':
+    pesoIdeal = (62.1 * altura) - 44.7
+    print(f'Seu peso ideal é {pesoIdeal}')
