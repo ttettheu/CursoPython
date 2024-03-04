35c10dcfc8edfed68dc0a13b7e480927199b0a3c
matheus <matheusalcantarapvh@gmail.com>
2024-03-04 10:13:40 -0300

    Exercício 014

--- secao_005/exercicios/ex_014.py
+++ secao_005/exercicios/ex_014.py
@@ -9,3 +9,15 @@ e a um exame final. A média das três notas mencionadas anteriormente obedece a
 Conforme o resultado, mostre na tela se o aluno está reprovado (média entre 0 e 2,9), de
 recuperação (entre 3 e 4,9) ou se foi aprovado. Faça todas as verificações necessárias.
 """
+
+notaLaboratorio = float(input('Nota de trabalho de laboratório: '))
+notaAvaliacao = float(input('Nota da Avaliação Semestral: '))
+notaExame = float(input('Nota Exame Final: '))
+media = (notaLaboratorio * 2) + (notaAvaliacao * 3) + (notaExame * 5) / 10
+
+if media > 0 and media <= 2.9:
+    print(f'{media} - Reprovado')
+elif media > 3 and media <= 4.9:
+    print(f'{nota} - Recuperação')
+else:
+    print(f'{media} - Aprovado')
