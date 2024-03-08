8f15e429e4a7068ee22072343c4feed17cb85e39
matheus <matheusalcantarapvh@gmail.com>
2024-03-08 12:39:31 -0300

    Exercício 015

--- secao_005/exercicios/ex_015.py
+++ secao_005/exercicios/ex_015.py
@@ -6,21 +6,22 @@ assim por diante.
 
 numero = int(input('Digite um número inteiro entre 1 e 7: '))
 
-if numero == 1:
-    print('Domindo')
-elif numero == 2:
-    print('Segunda-feira')
-elif numero == 3:
-    print('Terça-feira')
-elif numero == 4:
-    print('Quarta-feira')
-elif numero == 5:
-    print('Quinta-feira')
-elif numero == 6:
-    print('Sexta-feira')
-elif numero == 7:
-    print('Sábado')
-else:
-    print(f'O {numero} não é um número entre 1 e 7, tente novamente!')
+match numero:
+    case 1:
+        print('Domindo')
+    case 2:
+        print('Segunda-feira')
+    case 3:
+        print('Terça-feira')
+    case 4:
+        print('Quarta-feira')
+    case 5:
+        print('Quinta-feira')
+    case 6:
+        print('Sexta-feira')
+    case 7:
+        print('Sábado')
+    case _:
+        print(f'{numero} não é um número entre 1 e 7, tente novamente!')
 
-# Não usei switch, não foi ensinado na aula da seção 5. E o programa funciona mesmo sem ele.
+# Agora usei switch, não foi ensinado na aula da seção 5. Pesquisei e apliquei.
