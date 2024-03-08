edb53d55a32db69f12a3b233302a62c88870ee3c
matheus <matheusalcantarapvh@gmail.com>
2024-03-08 12:54:28 -0300

    Exercício 017

--- secao_005/exercicios/ex_017.py
+++ secao_005/exercicios/ex_017.py
@@ -3,3 +3,13 @@ Faça um programa que calcule e mostre a área de um trapézio. Sabe-se que:
     A = (basemaior + basemenor) * altura / 2
 Lembre-se a base maior e base menor devem ser número maiores que zero.
 """
+print('Área de um Trapézio')
+baseMaior = float(input('Digite o valor da base maior: '))
+baseMenor = float(input('Digite o valor da base menor: '))
+altura = float(input('Digite o valor da altura: '))
+A = (baseMaior + baseMenor) * altura / 2
+
+if baseMaior > 0 and baseMenor > 0:
+    print(f'A área do trapézio é {A}')
+else:
+    print(f'A base maior e a base menor devem ser números maiores que zero.')
