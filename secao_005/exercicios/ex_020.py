0fd067d04c4a2a11a862035ecff83531c098d8e6
matheus <matheusalcantarapvh@gmail.com>
2024-03-10 21:08:10 -0300

    Exercício 020

--- secao_005/exercicios/ex_020.py
+++ secao_005/exercicios/ex_020.py
@@ -4,5 +4,16 @@ se é um triângulo escaleno, equilátero ou isóscele, considerando os seguinte
     - O comprimento de cada lado de um triângulo com três lados iguais.
     - Chama-se equilátero o triângulo com três lados iguais.
     - Denomina-se isósceles o triângulo com o comprimento de dois lados iguais.
-    - Recebe o noe de escaleno o triângulo com os três lados diferentes.
+    - Recebe o nome de escaleno o triângulo com os três lados diferentes.
 """
+
+a = float(input('Digite o valor do lado A: '))
+b = float(input('Digite o valor do lado B: '))
+c = float(input('Digite o valor do lado C: '))
+
+if a == b == c:
+    print('É um triângulo equilátero.')
+elif a == b or a == c or b == c:
+    print('É um triângulo isósceles.')
+elif a != b and a != c and b != c:
+    print('É um triângulo escaleno.')
