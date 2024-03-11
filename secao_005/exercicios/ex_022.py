19659a15c22dfd07a3222f44831baf61aa6eaafe
matheus <matheusalcantarapvh@gmail.com>
2024-03-10 22:04:42 -0300

    Exercício 022

--- secao_005/exercicios/ex_022.py
+++ secao_005/exercicios/ex_022.py
@@ -5,3 +5,11 @@ aposentar. As condições para aposentadoria são:
     - Ou ter trabalhado pelo menos 30 anos,
     - Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.
 """
+
+idade = int(input('Digite sua idade: '))
+tempoDeServico = int(input('Digite o tempo de serviço: '))
+
+if idade >= 65 or tempoDeServico >= 30 or idade >= 60 and tempoDeServico >= 25:
+    print('Você pode se aposentar.')
+else:
+    print('Você não pode se aposentar.')
