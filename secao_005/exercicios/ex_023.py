0e21c43455fa8f0610c289513f2e9ca4e220d46a
matheus <matheusalcantarapvh@gmail.com>
2024-03-10 22:09:14 -0300

    Exercício 023

--- secao_005/exercicios/ex_023.py
+++ secao_005/exercicios/ex_023.py
@@ -3,3 +3,10 @@ Determine se um determinado ano lido é bissexto. Sendo que um ano é bissexto s
 for divisível por 400 ou se for divisível por 4 e não for divisível por 100. Por exemplo:
 1988, 1992, 1996.
 """
+
+ano = int(input('Digite um ano: '))
+
+if (ano % 400 == 0) or (ano % 4 == 0 and ano % 100 != 0):
+    print(f'O ano {ano} é bissexto.')
+else:
+    print(f'O ano {ano} não é bissexto.')
