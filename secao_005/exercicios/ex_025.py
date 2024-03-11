e1013481fc25efc1fbad28b3b9ccfd39bb82200b
matheus <matheusalcantarapvh@gmail.com>
2024-03-10 23:26:52 -0300

    Exercício 025

--- secao_005/exercicios/ex_025.py
+++ secao_005/exercicios/ex_025.py
@@ -14,3 +14,21 @@ A variável 'a' tem que ser diferente de zero. Caso seja igual, imprima a mensag
     - Se Δ = 0, existe uma raiz real. Imprima a raiz e a mensagem 'Raiz única'.
     - Se Δ >= 0, imprima as duas raízes reais.
 """
+
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+c = float(input('Digite o valor de c: '))
+
+if a == 0:
+    print('Não é equação de segundo grau.')
+else:
+    delta = b**2 - 4*a*c
+
+    if delta < 0:
+        print('Não existe raiz')
+    elif delta == 0:
+        print('Raiz única.')
+    elif delta >= 0:
+        raiz1 = (-b + delta**(0.5)) / (2*a)
+        raiz2 = (-b - delta**(0.5)) / (2*a)
+        print(f'As raízes da equação são: x¹ = {raiz1} e x² = {raiz2}')
