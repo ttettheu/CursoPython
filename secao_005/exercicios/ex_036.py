ca6e8f41eb8457fda85c1eec167804b90b41b2bc
matheus <matheusalcantarapvh@gmail.com>
2024-03-11 19:48:01 -0300

    Exercício 036

--- secao_005/exercicios/ex_036.py
+++ secao_005/exercicios/ex_036.py
@@ -11,3 +11,23 @@ paga ao vendedor. Para calcular a comissão, considere a tabela abaixo:
     | Menor que R$ 20.000,00                                  | R$ 400,00 + 14% das vendas |
 
 """
+
+venda = float(input('Digite o valor da venda mensal: '))
+comissao = None
+
+if venda > 0:
+    if venda < 20_000:
+        comissao = 400 + 400 * 0.14
+    elif venda < 40_000:
+        comissao = 500 + 500 * 0.14
+    elif venda < 60_000:
+        comissao = 550 + 550 * 0.14
+    elif venda < 80_000:
+        comissao = 600 + 600 * 0.14
+    elif venda < 100_000:
+        comissao = 650 + 650 * 0.14
+    elif venda >= 100_000:
+        comissao = 700 + 700 * 0.16
+    print(f'Você receberá uma comissão de {comissao}')
+else:
+    print(f'Digite o valor de venda válido.')
