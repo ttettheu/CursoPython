5431c59c11edeb3730d0c0e295bb5e044a7e8767
matheus <matheusalcantarapvh@gmail.com>
2024-03-11 10:53:54 -0300

    Exercício 027

--- secao_005/exercicios/ex_027.py
+++ secao_005/exercicios/ex_027.py
@@ -10,3 +10,18 @@ seguintes categorias:
     | Sênio      | maiores de 18 anos |
 
 """
+
+idade = int(input('Digite a idade: '))
+
+if idade >= 5 and idade <= 7:
+    categoria = 'Infantil A'
+elif idade >= 8 and idade <= 10:
+    categoria = 'Infantil B'
+elif idade >= 11 and idade <= 13:
+    categoria = 'Juvenil A'
+elif idade >= 14 and idade <= 17:
+    categoria = 'Juvenil B'
+elif idade >= 18:
+    categoria = 'Sênior'
+
+print(f'O nanador está na categoria: {categoria}')
