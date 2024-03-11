5e21d8c5e0952059941b14957e30e3f0c1909564
matheus <matheusalcantarapvh@gmail.com>
2024-03-11 15:18:11 -0300

    Exercício 030

--- secao_005/exercicios/ex_030.py
+++ secao_005/exercicios/ex_030.py
@@ -1,3 +1,9 @@
 """
 Faça um programa que receba três números e mostre-os em ordem crescente.
 """
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+c = int(input('Digite o terceiro número: '))
+numeros = [a, b, c]
+
+print(f'Número em ordem crescente: {sorted(numeros)}')
