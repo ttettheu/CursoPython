9e85083e20c72cfcfcedc9f08cb2df31fbc4a8cb
matheus <matheusalcantarapvh@gmail.com>
2024-03-11 10:41:47 -0300

    Exercício 026

--- secao_005/exercicios/ex_026.py
+++ secao_005/exercicios/ex_026.py
@@ -1,7 +1,6 @@
 """
 Leia a distância em Km e a quantidade de litros de gasolina consumidos por um carro
-em um percurso, calcule o consumo em Km/l e escreva uma mensagem de acordo com a
-tabela abaixo:
+em um percurso, calcule o consumo em Km/l e escreva uma mensagem conforme a tabela abaixo:
 
     | CONSUMO   | (Km/l) | MENSAGEM         |
     | menor que |   8    | Venda o carro!   |
@@ -9,3 +8,15 @@ tabela abaixo:
     | maior que |   12   | Super econômico! |
 
 """
+
+distancia = float(input('Digite a distância em km: '))
+litros = float(input('Digite a quantidade de litros de gasolina consumidos: '))
+
+consumo = distancia / litros
+
+if consumo < 8:
+    print(f'Consumo: {consumo:.2f} Km/l - Venda o carro!')
+elif consumo >= 8 and consumo <= 14:
+    print(f'Consumo: {consumo:.2f} Km/l - Econômico!')
+else:
+    print(f'Consumo: {consumo:.2f} Km/l - Super economico')
