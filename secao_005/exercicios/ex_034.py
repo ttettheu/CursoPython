b37b92550810be54f8ef1c0ab173a1a7b1e606bd
matheus <matheusalcantarapvh@gmail.com>
2024-03-11 19:01:56 -0300

    Exercício 034

--- secao_005/exercicios/ex_034.py
+++ secao_005/exercicios/ex_034.py
@@ -10,3 +10,32 @@ tabela abaixo, quando o aluno tem mais de 20 faltas ocorre uma redução de conc
     | 0.0 até 3.0  |            E             |              E               |
 
 """
+
+nota = float(input('Digite a nota: '))
+faltas = int(input('Digite o número de faltas: '))
+
+if nota >= 0:
+    if nota >= 9 and nota <= 10:
+        conceito = 'A'
+    elif nota >= 7.5:
+        cocneito = 'B'
+    elif nota >= 5:
+        cocneito = 'C'
+    elif nota >= 4:
+        cocneito = 'D'
+    else:
+        cocneito = 'E'
+else:
+    print('Digite uma nota válida. (Entre 1 e 10)')
+
+if faltas >= 0:
+    if faltas > 20:
+        if nota >= 9 and nota <= 10:
+            conceito = 'B'
+        elif nota >= 7.5:
+             cocneito = 'C'
+        elif nota >= 5:
+            cocneito = 'D'
+        elif nota >= 4:
+            cocneito = 'E'
+    print(f'Nota: {nota} Faltas: {faltas} - Conceito: {conceito}')
